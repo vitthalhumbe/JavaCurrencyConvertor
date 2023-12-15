dba095dbffe2e46c085d82b816077b7f31dd7bbc
Vitthal <vitthal.3454@gmail.com>
2023-12-15 14:48:42 +0530

    Added Logic of conversion and it's completed

--- CurrencyConvertor.java
+++ CurrencyConvertor.java
@@ -2,6 +2,8 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -75,12 +77,106 @@ public class CurrencyConvertor {
         JButton convertButton = new JButton("Convert");
         convertButton.setBackground(Color.decode("#5DA1E0"));
         convertButton.setBounds(155, 407, 300, 50);
+        convertButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputType = String.valueOf(fromCurrency.getSelectedItem());
+                String outputType = String.valueOf(toCurrency.getSelectedItem());
+
+                try {
+                    double inputAmount = Double.parseDouble(inputTextField.getText());
+
+                    if (inputType.equals("Indian Rupees")) {
+                        if (outputType.equals("American Dollar")) {
+                            double outputValue = rupeeToDollars(inputAmount);
+                            outputTextLabel.setText(" $ " + String.valueOf(outputValue));
+                        }
+                        else if (outputType.equals("Japanese Yen")) {
+                            double outputValue = rupeeToYen(inputAmount);
+                            outputTextLabel.setText(" ¥ " + String.valueOf(outputValue));
+                        }
+                        else if (outputType.equals("European Euro")) {
+                            double outputValue = rupeeToEuro(inputAmount);
+                            outputTextLabel.setText(" € " + String.valueOf(outputValue));
+                        }
+                        else {
+                            outputTextLabel.setText(" ₹ " + inputAmount);
+                        }
+                    }
+                    else if (inputType.equals("American Dollar")) {
+                        if (outputType.equals("Indian Rupees")) {
+                            double outputValue = dollarToRupee(inputAmount);
+                            outputTextLabel.setText(" ₹ " + String.valueOf(outputValue));
+                        }
+                        else if (outputType.equals("Japanese Yen")) {
+                            double outputValue = dollarToYen(inputAmount);
+                            outputTextLabel.setText(" ¥ " + String.valueOf(outputValue));
+                        }
+                        else if (outputType.equals("European Euro")) {
+                            double outputValue = dollarToEuro(inputAmount);
+                            outputTextLabel.setText(" € " + String.valueOf(outputValue));
+                        }
+                        else {
+                            outputTextLabel.setText(" $ " + inputAmount);
+                        }
+                    }
+                    else if (inputType.equals("Japanese Yen")) {
+                        if (outputType.equals("Indian Rupees")) {
+                            double outputValue = yenToRupee(inputAmount);
+                            outputTextLabel.setText(" ₹ " + String.valueOf(outputValue));
+                        }
+                        else if (outputType.equals("American Dollar")) {
+                            double outputValue = yenToDollar(inputAmount);
+                            outputTextLabel.setText(" $ " + String.valueOf(outputValue));
+                        }
+                        else if (outputType.equals("European Euro")) {
+                            double outputValue = yenToEuro(inputAmount);
+                            outputTextLabel.setText(" € " + String.valueOf(outputValue));
+                        }
+                        else {
+                            outputTextLabel.setText(" ¥ " + inputAmount);
+                        }
+                    }
+                    else {
+                        if (outputType.equals("Indian Rupees")) {
+                            double outputValue = euroToRupee(inputAmount);
+                            outputTextLabel.setText(" ₹ " + String.valueOf(outputValue));
+                        }
+                        else if (outputType.equals("American Dollar")) {
+                            double outputValue = euroToDollar(inputAmount);
+                            outputTextLabel.setText(" $ " + String.valueOf(outputValue));
+                        }
+                        else if (outputType.equals("Japanese Yen")) {
+                            double outputValue = euroToYen(inputAmount);
+                            outputTextLabel.setText(" € " + String.valueOf(outputValue));
+                        }
+                        else {
+                            outputTextLabel.setText(" € " + inputAmount);
+                        }
+                    }
+
+                }
+                catch(NumberFormatException nfe) {
+                    JOptionPane.showMessageDialog(frame, "Enter only numbers");
+                }
+                catch(Exception a) {
+                    System.out.println(a);
+                }
+            }
+        });
         frame.add(convertButton);
 
         // Clear Button
         JButton clearButton = new JButton("Clear");
         clearButton.setBackground(Color.white);
         clearButton.setBounds(504, 407, 300, 50);
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputTextField.setText("");
+                outputTextLabel.setText("");
+            }
+        });
         frame.add(clearButton);
 
         frame.setVisible(true);
@@ -88,5 +184,42 @@ public class CurrencyConvertor {
 
     public static void main(String[] args) {
         new CurrencyConvertor();
+    }
+
+    private double rupeeToDollars(double rupee) {
+        return rupee * 0.012;
+    }
+    private double rupeeToYen(double rupee) {
+        return rupee * 1.74457;
+    }
+    private double rupeeToEuro(double rupee) {
+        return rupee * 0.011;
+    }
+    private double dollarToRupee(double dollar) {
+        return dollar * 83.09;
+    }
+    private double dollarToYen(double dollar) {
+        return dollar * 141.66;
+    }
+    private double dollarToEuro(double dollar) {
+        return dollar * 0.918624;
+    }
+    private double yenToRupee(double yen) {
+        return yen * 0.59;
+    }
+    private double yenToDollar(double yen) {
+        return yen * 0.0071;
+    }
+    private double yenToEuro(double yen) {
+        return yen * 0.0064;
+    }
+    private double euroToRupee(double euro) {
+        return euro * 91.01;
+    }
+    private double euroToDollar(double euro) {
+        return euro * 1.10;
+    }
+    private double euroToYen(double euro) {
+        return euro * 155.25;
     }
 }
